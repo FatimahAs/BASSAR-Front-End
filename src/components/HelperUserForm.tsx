67f370702b0a758f9916c95a8d06009258509801
FATIMAH ASIRI <des.morjane@gmail.com>
2025-07-02 20:21:59 +0300

    fixed and edit conflects

--- src/components/HelperUserForm.tsx
+++ src/components/HelperUserForm.tsx
@@ -64,7 +64,7 @@ export default function HelperUserForm() {
           const data = await res.json();
           navigate(`/helper/page/${data.id}`);
         },
-        (err) => {
+        () => {
           setError("يرجى السماح بمشاركة الموقع الجغرافي.");
           setLoading(false);
         }
